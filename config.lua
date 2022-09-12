b04693e175b969f948e3a0de83b35616c396a5a5
SYNO <89760730+SYNO-Dev@users.noreply.github.com>
2022-09-12 08:21:12 +0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,18 +2,19 @@ Config = {}
 ------------------------------------------------------------------------------------------------------
 ------------------------------------------ CONFIG ---------------------------------------------
 ------------------------------------------------------------------------------------------------------ 
-Config.EnableBlips = true        --enables blip in the map
-Config.EnablePeds = true         --enables poeds
-Config.UseRprogress = false      --if true you need Rprogressbar
-Config.moneytype = 'bank'        --cash type 'money','bank',black_money'
-Config.Price = 5000              --Price for relog
-Config.ped = "cs_bankman"        --ped models  link : https://wiki.rage.mp/index.php?title=Peds
-Config.pedhash = 0x9760192E      --ped hash
-Config.DrawText = true           --If false you need renzu_popui
+Config.EnableBlips       = true           -- Enables blip in the map
+Config.EnablePeds        = true           -- Enables poeds
+Config.UseRprogress      = false          -- If true you need Rprogressbar
+Config.moneytype         = 'bank'         -- Cash type 'money','bank',black_money'
+Config.Price             = 5000           -- Price for relog
+Config.ped               = "cs_bankman"   -- Ped models  link : https://wiki.rage.mp/index.php?title=Peds
+Config.pedhash           = 0x9760192E     -- Ped hash
+Config.DrawText          = true           -- If false you need renzu_popui
+Config.Duration          = 10             -- The ProgressBar Duration
 --BLIPS--
-Config.Sprite = 84               --https://docs.fivem.net/docs/game-references/blips/
-Config.Scale = 0.6
-Config.color = 2                 --https://docs.fivem.net/docs/game-references/blips/#blip-colors
+Config.Sprite            = 84               --https://docs.fivem.net/docs/game-references/blips/
+Config.Scale             = 0.6
+Config.color             = 2                 --https://docs.fivem.net/docs/game-references/blips/#blip-colors
 Config.AKbot = {
     {x = -2031.21, y = -464.87, z = 11.6, heading = 229.14}
    -- {x = , y = , z = , heading = xx}                        --Add more bot locations here
